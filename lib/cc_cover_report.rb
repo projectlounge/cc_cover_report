0132aed2a89f8a39402aac4ed9122c7c6b600341
Ian Connor <iconnor@projectlounge.com>
2015-01-12 18:33:24 -0800

    Allow expired certs to work

--- lib/cc_cover_report.rb
+++ lib/cc_cover_report.rb
@@ -1,11 +1,11 @@
 require "cc_cover_report/version"
+require 'rest_client'
 
 class CcCoverReport
   def self.report(repo_key = nil)
     begin
       repo_key ||= ENV['CC_REPO_KEY']
       if repo_key.present?
-        require 'rest_client'
         index_file = SimpleCov.coverage_path + "/index.html"
         if SimpleCov.running
           silence_stream(STDOUT) do
@@ -13,18 +13,20 @@ class CcCoverReport
           end
         end
         if File.exist?(index_file)
-          RestClient.post('https://codecop.encore.io/api/coverage.json', 
-          :commit => `git log --pretty=%P -n 1`.chomp,
-          :repo_key => repo_key,
-          :coverage_file => File.new(index_file)
-          )
-          puts "Pushed coverage report to CC"
+          RestClient::Request.execute(:url => 'https://codecop.encore.io/api/coverage.json', :method => :post, :verify_ssl => false,
+          :payload => {
+            :commit => `git log --pretty=%P -n 1`.chomp,
+            :repo_key => repo_key,
+            :coverage_file => File.new(index_file)}
+            )
+            puts "Pushed coverage report to CC"
+          end
+        else
+          # No index file found - cannot upload coverage report
         end
-      else
-        # No index file found - cannot upload coverage report
+      rescue Exception
+        # Don't break any builds - just fail
       end
-    rescue Exception
-      # Don't break any builds - just fail
     end
   end
-end
+  
